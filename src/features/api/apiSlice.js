834d9164ef0f6adfc0d5676a85096e9e6ebe448f
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2023-03-24 01:48:29 +0600

    authApi and authSlice done

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -1,9 +1,9 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { createApi, fakeBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
   reducerPath: "api",
-  baseQuery: fetchBaseQuery({
-    baseUrl: process.env.REACT_APP_API_URL,
+  baseQuery: fakeBaseQuery({
+    baseQuery: process.env.REACT_APP_API_URL,
   }),
   tagTypes: [],
   endpoints: (builder) => ({}),
