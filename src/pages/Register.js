1082e36f2fdaaa97c1104e0f70b3635dfc30660a
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2023-03-26 00:18:18 +0600

    handle function done

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -137,6 +137,7 @@ export default function Register() {
               <button
                 type="submit"
                 className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-violet-600 hover:bg-violet-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-violet-500"
+                disabled={isLoading}
               >
                 <span className="absolute left-0 inset-y-0 flex items-center pl-3"></span>
                 Sign up
